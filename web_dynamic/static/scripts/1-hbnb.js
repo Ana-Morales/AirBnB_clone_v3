22813e05e553cea2f8ea3b6fd612c2119afc0a2d
Ana-Morales <moralesanaruth@gmail.com>
2021-02-13 01:53:28 +0000

    Fix typo

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
-    const dicAmenities = {};
-    $(document).on('change', "input[type='checkbox']", function () {
-        if (this.checked) {
-            dicAmenities[$(this).data('id')] = $(this).data('name');
-        } else {
-            delete dicAmenities[$(this).data('id')];
-        }
-        const lst = Object.values(dicAmenities);
-        if (lst.length > 0) {
-            $('div.amenities > h4').text(lst.join(', '));
-        } else {
-            $('div.amenities > h4').html('&nbsp;');
-        }
-    });
+  const dicAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked === true) {
+      dicAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete dicAmenities[$(this).data('id')];
+    }
+    const lst = Object.values(dicAmenities);
+    $('div.amenities h4').text(lst.join(', '));
+  });
 });
