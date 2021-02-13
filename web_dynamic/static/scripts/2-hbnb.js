edd77d41ad3a7800af30b0672a5706485d156148
Ana-Morales <moralesanaruth@gmail.com>
2021-02-13 04:09:24 +0000

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  const dicAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked === true) {
+      dicAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete dicAmenities[$(this).data('id')];
+    }
+    const lst = Object.values(dicAmenities);
+    $('div.amenities h4').text(lst.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
