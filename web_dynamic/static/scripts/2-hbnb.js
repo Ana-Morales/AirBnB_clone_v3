df4c1a6171131944cc730f449064dbb89d0748a0
Ana-Morales <moralesanaruth@gmail.com>
2021-02-15 14:13:07 +0000

    Fix path

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     const lst = Object.values(dicAmenities);
     $('div.amenities h4').text(lst.join(', '));
   });
-  $.get('http://localhost:5001/api/v1/status', function (data, textStatus) {
+  $.get('http://0.0.0.0:5001/api/v1/status', function (data, textStatus) {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
